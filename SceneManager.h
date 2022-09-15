e84c2bc86c9f78458b83303242cef4128322fd17
Noemi Banks <110627407+mimimalloc@users.noreply.github.com>
2022-09-15 14:55:30 -0400

    SceneManager comments

--- SceneManager.h
+++ SceneManager.h
@@ -3,22 +3,43 @@
 #include <queue>
 #include "IScene.h"
 
+/**
+	[ SceneManager ]
+	A SceneManager is responsible for holding, updating, and drawing 
+	its scenes. Scenes are updated from front to back, but they are
+	drawn from back to front, so that scenes on top of the deque
+	are drawn on top of other scenes.
+*/
 class SceneManager
 {
 public:
 	SceneManager() {};
 	~SceneManager();
 
+	// Adds a scene to the front of the deque
 	void AddFrontScene(IScene* scene);
+
+	// Adds a scene to the back of the deque
 	void AddBackScene(IScene* scene);
+
+	// Removes and returns a pointer to a scene from the front of the deque.
 	IScene* PopScene();
+
+	// Removes all scenes from the deque and frees their memory
 	void ClearAllScenes();
+
+	// Removes all scenes from the deque and frees their memory,
+	// except for the scene in front
 	void ClearBackScenes();
+
+	// Returns true if the deque is empty
 	inline bool IsEmpty() { return scenes.empty(); }
 
 	void Update(float dt);
 	void Draw();
 private:
+	// The deque that contains pointers to all the scenes
+	// the instance manages
 	std::deque<IScene*> scenes;
 };
 
