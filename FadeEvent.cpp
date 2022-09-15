d04a1f5ff29bb827900a2e61a13f363ab18fbfd5
Noemi Banks <110627407+mimimalloc@users.noreply.github.com>
2022-09-15 14:20:00 -0400

    FadeEvent comments

--- FadeEvent.cpp
+++ FadeEvent.cpp
@@ -15,14 +15,18 @@ void FadeEvent::Start()
 void FadeEvent::Run(float dt)
 {
 	if (direction == fadeout) {
+		// To fadeout, lower the Entity's opacity by (rate * dt)
 		fadeEntity->SetOpacity(fadeEntity->GetOpacity() - rate * dt);
+		// When it reaches 0.0f, the FadeEvent has finished
 		if (fadeEntity->GetOpacity() <= 0.0f) {
 			fadeEntity->SetOpacity(0.0f);
 			isRunning = false;
 			isFinished = true;
 		}
 	} else {
+		// To fadein, increase the Entity's opacity by (rate * dt)
 		fadeEntity->SetOpacity(fadeEntity->GetOpacity() + rate * dt);
+		// When it reaches 1.0f, the FadeEvent has finished
 		if (fadeEntity->GetOpacity() >= 1.0f) {
 			fadeEntity->SetOpacity(1.0f);
 			isRunning = false;
--- FadeEvent.h
+++ FadeEvent.h
@@ -2,11 +2,24 @@
 #include "EventSystem.h"
 #include "Entity.h"
 
+/**
+	[ FadeDirection ]
+	Determines whether the transition increases or
+	decreases an Entity's opacity
+*/
 enum FadeDirection {
 	fadeout,
 	fadein
 };
 
+
+/**
+	[ FadeEvent]
+	This is an event that stores a pointer to an Entity,
+	a direction to fade opacity in or out, and the rate at
+	which to increase or decrease the opacity of the Entity
+	each iteration of the game loop.
+*/
 class FadeEvent : public Event
 {
 public:
@@ -15,8 +28,13 @@ public:
 	void Run(float dt);
 	void End() {}
 private:
+	// Pointer to the Entity being acted upon
 	Entity* fadeEntity;
+
+	// Whether the Entity is "faded" in or out
 	FadeDirection direction;
+
+	// The rate at which the Entity fades
 	float rate;
 };
 
