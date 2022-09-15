4f7f2c3dea9dcb09074dc2500b3891b6f85c7cb3
Noemi Banks <110627407+mimimalloc@users.noreply.github.com>
2022-09-15 14:47:38 -0400

    IScene comments

--- IScene.h
+++ IScene.h
@@ -1,7 +1,16 @@
 #pragma once
 
+/**
+	[ IScene ]
+	The base scene that other scenes inherit from.
+
+	Don't use this class on its own - it's intended
+	to be inherited by "real" Scenes.
+*/
 class IScene {
 protected:
+	// This is true when the scene is done, and is
+	// ready for its SceneManager to destroy it
 	bool readyForRelease{ false };
 public:
 	IScene() {}
