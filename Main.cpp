1f19d4ddc3ed9940e299c8c8c4e071a873b73b7a
Noemi Banks <110627407+mimimalloc@users.noreply.github.com>
2022-09-15 11:59:17 -0400

    Main.cpp comments

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,26 @@
+/**
+	Wrong!
+
+	It's like Pong, but Wrong!
+	Avoid touching the ball with your paddle.
+
+	Whoever reaches 10 points first wins!
+
+	This is a learning project. Many aspects of this program
+	are overcomplicated for the sake of trying something that
+	might be more extensible than hard-coding everything.
+
+	This game by Noemi Banks (mimimalloc)
+*/
+
 #include "raylib.h"
 #include "Game.h"
-#include "EventSystem.h"
 
 int main() {
 	Game* game = new Game();
 
+	// Initialize and run the game. Run manages the two game loops,
+	// so when it ends it's time to prepare to exit.
 	game->Initialize();
 	game->Run();
 	game->Exit();
--- SceneManager.cpp
+++ SceneManager.cpp
@@ -50,6 +50,7 @@ void SceneManager::Update(float dt)
 
 void SceneManager::Draw()
 {
+	// Scenes are drawn from the bottom up - that means that scenes on the top are drawn on top
 	if (!scenes.empty()) {
 		for (int i = scenes.size() - 1; i >= 0; i--) {
 			scenes.at(i)->Draw();
