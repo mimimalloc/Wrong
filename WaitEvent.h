cde46c4841681d61ca23f8f3590b97e9cadc5488
Noemi Banks <110627407+mimimalloc@users.noreply.github.com>
2022-09-15 14:31:51 -0400

    WaitEvent comments

--- WaitEvent.h
+++ WaitEvent.h
@@ -1,5 +1,11 @@
 #pragma once
 #include "EventSystem.h"
+
+/**
+	[ WaitEvent ]
+	This event does one thing and one thing only - waits for a duration
+	in approximate seconds before finishing.
+*/
 class WaitEvent : public Event
 {
 public:
@@ -8,6 +14,9 @@ public:
 	void Run(float dt);
 	void End() {}
 private:
+	// How long the event has been active
 	float currentWaitTime;
+
+	// The target amount of time, after which the event will be finished
 	float targetWaitTime;
 };
