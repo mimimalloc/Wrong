d98ea084995579222905bc8bec94a9d3c9cba547
Noemi Banks <110627407+mimimalloc@users.noreply.github.com>
2022-09-15 14:13:08 -0400

    OverlayEntity comments

--- OverlayEntity.h
+++ OverlayEntity.h
@@ -2,6 +2,13 @@
 #include "Entity.h"
 #include "raylib.h"
 
+/**
+	[ OverlayEntity ]
+	This is one of the simplest possible entities,
+	a box that is drawn to cover the screen and hide
+	elements that are drawn behind it. Its opacity can
+	be manipulated for fade effects.
+*/
 class OverlayEntity : public Entity
 {
 public:
