fe1c3585d2037ed97855d0823878014ce5030ddf
Noemi Banks <110627407+mimimalloc@users.noreply.github.com>
2022-09-16 14:35:23 -0400

    TextEntity comments

--- TextEntity.h
+++ TextEntity.h
@@ -3,11 +3,19 @@
 #include <string>
 #include "raylib.h"
 
+/**
+	[ TextEntity ]
+	An entity that draws some text to the screen.
+	This text can be modified in various ways
+	after the entity has been created through
+	setters.
+*/
 class TextEntity : public Entity
 {
 public:
 	TextEntity(std::string text, float x, float y, int size, Color color = WHITE, float opacity = 1.0f, Font font = GetFontDefault());
 
+	// Getters and setters
 	inline std::string GetMessageText() { return messageText; }
 	inline int GetFontSize() { return fontSize; }
 	inline Font GetFont() { return font; }
@@ -18,9 +26,13 @@ public:
 
 	void Draw();
 private:
+	// The text displayed
 	std::string messageText;
+	// The size of the font
 	int fontSize;
+	// The color of the text
 	Color color;
+	// The font being used
 	Font font;
 };
 
