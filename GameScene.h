8809d2227d5932783a6880f2463ba0a2db6f3a06
Noemi Banks <110627407+mimimalloc@users.noreply.github.com>
2022-09-21 16:27:44 -0400

    GameScene comments

--- GameScene.h
+++ GameScene.h
@@ -4,6 +4,13 @@
 #include "SceneManager.h"
 #include "EntityManager.h"
 #include "raylib.h"
+
+/**
+	[ GameScene ]
+	Scene that runs the game itself.
+	Manages the two paddles, the ball,
+	and collisions between these and the walls
+*/
 class GameScene : public IScene
 {
 public:
@@ -14,9 +21,13 @@ public:
 	void Draw();
 	bool Update(float dt);
 private:
+	// Whether the scene suppresses later scene's update calls
 	bool suppressUpdates;
+	// The main EventQueue
 	EventQueue* eventQueue;
+	// The main SceneManager
 	SceneManager* sceneManager;
+	// This scene's EntityManager
 	EntityManager* entityManager;
 };
 
