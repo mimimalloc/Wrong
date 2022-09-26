9829feed4d0909930ac982909420b79a4f818303
Noemi Banks <110627407+mimimalloc@users.noreply.github.com>
2022-09-26 16:34:08 -0400

    WinnerScene comments

--- WinnerScene.h
+++ WinnerScene.h
@@ -4,6 +4,13 @@
 #include "SceneManager.h"
 #include <string>
 #include "EntityManager.h"
+
+/**
+	[ WinnerScene ]
+	This scene occurs on top of the game scene
+	when a player reaches a score of 10. It blocks
+	the game scene from updating until it is finished.
+*/
 class WinnerScene : public IScene
 {
 public:
@@ -15,10 +22,15 @@ public:
 	void Draw();
 
 private:
+	// The font of the "winner" text
 	Font font;
+	// The winner of the game as a string
 	std::string winner;
+	// The main event queue
 	EventQueue* eventQueue;
+	// The main scene manager
 	SceneManager* sceneManager;
+	// This scene's entity manager
 	EntityManager* entityManager;
 };
 
