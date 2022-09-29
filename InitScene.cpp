e8b4ee5fcc9cebcefac800ea9724a404bd1cf0e2
Noemi Banks <110627407+mimimalloc@users.noreply.github.com>
2022-09-29 16:16:43 -0400

    InitScene placement, Raylib logo

--- InitScene.cpp
+++ InitScene.cpp
@@ -16,12 +16,19 @@ InitScene::~InitScene()
 
 void InitScene::Initialize()
 {
+	float xpos = game->Screen()->topLeft.x + 48;
+	float ypos = game->Screen()->topLeft.y + 80;
+
 	Font font = LoadFont("resources/RaccoonSerif-Monospace.ttf");
-	TextEntity* text1 = new TextEntity("Developed by", 40, 80, 24, WHITE, 1.0f, font);
-	TextEntity* text2 = new TextEntity("Noemi Banks (mimimalloc)", 60, 120, 32, WHITE, 1.0f, font);
+	TextEntity* text1 = new TextEntity("Developed by", xpos, ypos, 24, WHITE, 1.0f, font);
+	TextEntity* text2 = new TextEntity("Noemi Banks (mimimalloc)", xpos + 20, ypos + 40, 32, WHITE, 1.0f, font);
+
+	ImageEntity* rayLogo = new ImageEntity("resources/raylib_96x96.png", 0, 0, 96, 96);
+	rayLogo->SetPosCentered({ game->Screen()->botCenter.x, game->Screen()->botCenter.y - 240 });
 
 	entityManager->AddEntity("text1", text1);
 	entityManager->AddEntity("text2", text2);
+	entityManager->AddEntity("logo", rayLogo);
 	
 	IScene* newScene = new TitleScene(game);
 
